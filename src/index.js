2fad50b097a9c914de93d16ac2b96e698d295b06
Marco Caldera <marco.caldera@icloud.com>
2021-01-30 16:36:59 +0100

    Hotfix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { View, StyleSheet, Animated, TouchableOpacity } from "react-native";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { Animated, TouchableOpacity, View } from "react-native";
 
 const propTypes = {
   renderView: PropTypes.func.isRequired,
   renderCollapseView: PropTypes.func.isRequired,
   collapse: PropTypes.bool,
   tension: PropTypes.number,
-}
+};
 const defaultProps = {
   collapse: false,
-  tension: 10
+  tension: 10,
 };
 
 class CollapseView extends Component {
@@ -24,41 +24,51 @@ class CollapseView extends Component {
 
   collapse = () => {
     const { startpoint, endpoint, animation, collapse } = this.state;
-    let startAnim = collapse? endpoint + startpoint : startpoint;
-    let endAnim = collapse? startpoint : startpoint + endpoint;
+    let startAnim = collapse ? endpoint + startpoint : startpoint;
+    let endAnim = collapse ? startpoint : startpoint + endpoint;
     this.setState({
-      collapse: !this.state.collapse
-    })
-    
+      collapse: !this.state.collapse,
+    });
+
     animation.setValue(startAnim);
-    Animated.spring(
-      this.state.animation,
-      {
-        toValue: endAnim,
-        tension: this.props.tension,
-      }
-    ).start();
-  }
+    Animated.spring(this.state.animation, {
+      toValue: endAnim,
+      tension: this.props.tension,
+    }).start();
+  };
 
   startpoint = (layout) => {
-    if(!this.state.collapse) this.setState({animation: new Animated.Value(layout.nativeEvent.layout.height)});
+    if (!this.state.collapse)
+      this.setState({
+        animation: new Animated.Value(layout.nativeEvent.layout.height),
+      });
     this.setState({
-      startpoint: layout.nativeEvent.layout.height
-    })
-  }
+      startpoint: layout.nativeEvent.layout.height,
+    });
+  };
 
   endpoint = (layout) => {
-    if(this.state.collapse) this.setState({animation: new Animated.Value(layout.nativeEvent.layout.height)});
+    // if(this.state.collapse) this.setState({animation: new Animated.Value(layout.nativeEvent.layout.height)});
     this.setState({
       endpoint: layout.nativeEvent.layout.height,
-    })
-  }
+    });
+  };
 
   render() {
     const { startpoint, endpoint, animation, collapse } = this.state;
     return (
-      <Animated.View style={{height: this.state.animation, backgroundColor:'transparent', overflow: 'hidden'}}>
-        <TouchableOpacity activeOpacity={1} onPress={this.collapse} onLayout={this.startpoint}>
+      <Animated.View
+        style={{
+          height: this.state.animation,
+          backgroundColor: "transparent",
+          overflow: "hidden",
+        }}
+      >
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={this.collapse}
+          onLayout={this.startpoint}
+        >
           {this.props.renderView(this.state.collapse)}
         </TouchableOpacity>
         <View onLayout={this.endpoint}>
